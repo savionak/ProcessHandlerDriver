cf8a2230632b13d917e0aa43d26b10462e442ed5
SVss <savionak@gmail.com>
2016-12-10 00:32:22 +0300

    [ProcessHandlerKmdf]: Add UnloadDriver routine and register it

--- ProcessHandlerKmdf/DriverInit.c
+++ ProcessHandlerKmdf/DriverInit.c
@@ -4,14 +4,23 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT pDriverObject, IN PUNICODE_STRING pRegist
 {
 	NTSTATUS status = STATUS_SUCCESS;
 
-	UNREFERENCED_PARAMETER(pDriverObject);
 	UNREFERENCED_PARAMETER(pRegistryPath);
 
 	// Register major functions
 
 	// Register PCREATE_PROCESS_NOTIFY_ROUTINE
 
+	pDriverObject->DriverUnload = UnloadDriver;
+
 	return status;
+}
+
+
+VOID UnloadDriver(_In_ PDRIVER_OBJECT pDriverObject)
+{
+	UNREFERENCED_PARAMETER(pDriverObject);
+
+	// TODO: Clear driver resources
 }
 
 
--- ProcessHandlerKmdf/DriverInit.h
+++ ProcessHandlerKmdf/DriverInit.h
@@ -10,5 +10,8 @@
 // Driver initialization
 DRIVER_INITIALIZE DriverEntry;
 
+// Driver deinitialization
+DRIVER_UNLOAD UnloadDriver;
+
 // Process creation and termination callback
 VOID SetCreateProcessNotifyRoutine(_In_ HANDLE ParentId, _In_ HANDLE ProcessId, _In_ BOOLEAN isCreate);
